f17b9d86f13b3e29a539a44c345ba4354485a0b3
ahmedfahmida <fahmi.ahmed.2008@gmail.com>
2022-06-30 22:22:56 -0700

    Routing

--- tutorial-app/src/App.js
+++ tutorial-app/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import DataFetch from "./DataFetch";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Error from "./Pages/Error";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+//import DataFetch from "./DataFetch";
 
 //import { UseEffect } from "./UseEffect";
 
 
 export default function App() {
   return (
-    <div>
-        <DataFetch/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Blog" element={<Blog/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ tutorial-app/src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <p>Narrated by Death, the story follows Liesel Meminger, a young girl living with foster parents, Hans and Rosa Hubermann, 
+            in Nazi Germany during World War II. Liesel settles down into her new home and during her time there, she is exposed to 
+            the horrors of the war and politics. As the political situation in Germany deteriorates, her foster parents conceal a Jewish 
+            man named Max Vandenburg. Hans, who has developed a close relationship with Liesel, teaches her to read during this time. 
+            Recognizing the power of writing and sharing the written word, Liesel not only begins to steal books that the politicians are 
+            seeking to destroy, but also writes her own story, and shares the power of language with Max. By collecting laundry for her foster
+             mother, she also begins a relationship with the mayor's wife, Ilsa Hermann, who allows her to read books in her library and steal
+              them later.</p>
+    </div>
+  )
+}
+
+export default Home
